961ae3851cb111dd2edd1edbe71219fcc58fc390
AlexApostol <apostol516@gmail.com>
2025-08-15 09:25:21 +0200

    make it an actor

--- SmartLogging/Sources/SmartLogging/SmartLogging.swift
+++ SmartLogging/Sources/SmartLogging/SmartLogging.swift
@@ -4,55 +4,35 @@
 import OSLog
 import Foundation
 
-public protocol SmartLoggingProtocol {
-    func log(_ metadata: LogMetadata)
-    func config(config: SmartLoggingConfigProtocol)
-    func setGlobalLogLevel(_ level: LogLevel)
+public protocol SmartLoggingProtocol: Sendable {
+    func log(_ metadata: LogMetadata) async
+    /// Updates the logger instance in a thread-safe manner.
+    /// If a log is sent while the logger is being updated, the log will be handled by whichever
+    /// logger instance is active when the lock is acquired.
+    /// No logs will be lost or corrupted, but the specific logger used for concurrent operations cannot be guaranteed.
+    func config(config: SmartLoggingConfigProtocol) async
+    func setGlobalLogLevel(_ level: LogLevel) async
 }
 
-public protocol SmartLoggingConfigProtocol {
+public protocol SmartLoggingConfigProtocol: Sendable {
     var subsystem: Subsystem { get set }
 }
 
-public final class SmartLogging: SmartLoggingProtocol {
-    static let shared = SmartLogging()
-    private let lock: NSRecursiveLock = NSRecursiveLock()
-    private var _currentLogLevel: LogLevel = .default
-    private var _logger: SmartLogger
-    private var logger: SmartLogger {
-        get {
-            lock.lock()
-            defer { lock.unlock() }
-            return _logger
-        }
-        set {
-            lock.lock()
-            defer { lock.unlock() }
-            _logger = newValue
-        }
-    }
+public final actor SmartLogging: SmartLoggingProtocol {
+    public static let shared = SmartLogging()
+    private var logger: SmartLogger
     public var logLevel: LogLevel {
         return currentLogLevel
     }
-    private var currentLogLevel: LogLevel {
-        get {
-            lock.lock()
-            defer { lock.unlock() }
-            return _currentLogLevel
-        }
-        set {
-            lock.lock()
-            defer { lock.unlock() }
-            _currentLogLevel = newValue
-        }
-    }
+    private var currentLogLevel: LogLevel = .default
 
     private init(logger: SmartLogger = Logger()) {
-        self._logger = logger
+        self.logger = logger
     }
 
     /// Updates the logger instance in a thread-safe manner.
-    /// If a log is sent while the logger is being updated, the log will be handled by whichever logger instance is active when the lock is acquired.
+    /// If a log is sent while the logger is being updated, the log will be handled by whichever
+    /// logger instance is active when the lock is acquired.
     /// No logs will be lost or corrupted, but the specific logger used for concurrent operations cannot be guaranteed.
     public func config(config: SmartLoggingConfigProtocol) {
         logger = Logger(subsystem: config.subsystem.name, category: config.subsystem.category.name)
@@ -72,5 +52,3 @@ public final class SmartLogging: SmartLoggingProtocol {
         }
     }
 }
-
-
